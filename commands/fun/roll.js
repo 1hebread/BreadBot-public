504d5465d93023b402f3cc33cc5595a12184bf73
TheBread <zaharaaa220612@gmail.com>
2025-02-10 17:47:07 +0500

    Теперь канал берётся с конфига

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, Client } = require("discord.js");
+
+const client = new Client({
+    intents: []
+});
+
+client.config = require('../../config.json');
 
 module.exports = {
 
@@ -20,10 +26,10 @@ module.exports = {
         const max = interaction.options.getInteger('max') || 100;
 
         const random = Math.floor(Math.random() * (max - min + 1)) + min;
-        if (interaction.channel.id === "1294357469438541945" || interaction.user.id === "586408737712111616") {
+        if (interaction.channel.id === client.config.COMMANDS_CHANNEL_ID || interaction.user.id === client.config.OWNER_ID) {
             await interaction.reply({ content: `Выпало число **${random}**`, ephemeral: true });
         } else {
-            await interaction.reply({ content: "Эта команда доступна только в канале <#1294357469438541945>", ephemeral: true });
+            await interaction.reply({ content: `Эта команда доступна только в канале <#${client.config.COMMANDS_CHANNEL_ID}>`, ephemeral: true });
         }
     }
 };
